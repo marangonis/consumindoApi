dd58a179762fc1e45391857db56632196347fed7
Marangoni <marangonis@gmail.com>
2023-06-03 18:39:34 -0300

    Primeiro Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# js-consumindo-dados-api
+
+Repositório do treinamento "JavaScript: Consumindo e tratando dados de uma API" da Alura Cursos Online.
+"#consumindoApi" 
---
+++ script.js
@@ -0,0 +1,21 @@
+//
+// Fetch é uma Promisse
+
+async function buscaEndereco(cep) {
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error("CEP não existe");
+        }
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    } catch (erro) {
+        console.log(erro);
+    }
+}
+
+let ceps = ["01001000", "01001001"];
+let conjuntoCeps = ceps.map((valores) => buscaEndereco(valores));
+console.log(conjuntoCeps);
+Promise.all(conjuntoCeps).then((respostas) => console.log(respostas));
